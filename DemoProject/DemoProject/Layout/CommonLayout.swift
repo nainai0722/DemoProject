a294bccb6648c0e2abd5e9f0b5a0503f3faba9f1
Nana <tabularasa0722@gmail.com>
2025-02-04 20:40:36 +0900

    横幅修正
    左右合わせて100pxあけると、狭いので調整

--- DemoProject/DemoProject/Layout/CommonLayout.swift
+++ DemoProject/DemoProject/Layout/CommonLayout.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftUI
 
 struct CommonLayout {
-    static let width = UIDevice.current.userInterfaceIdiom == .phone ? UIScreen.main.bounds.width - 100 : UIScreen.main.bounds.width * 0.35
+    static let width = UIDevice.current.userInterfaceIdiom == .phone ? UIScreen.main.bounds.width - 20 : UIScreen.main.bounds.width * 0.35
 }
 
 struct QuizPageTopTitle: View {    
